a6facb8cf0a8d1fd8a1cc102e7889036c9a93b45
knelligan <ke10nell@siena.edu>
2022-04-16 22:49:10 -0400

    added button to forgot password

--- app/src/main/java/io/politicalempathy/ForgotPasswordActivity.java
+++ app/src/main/java/io/politicalempathy/ForgotPasswordActivity.java
@@ -3,6 +3,7 @@ package io.politicalempathy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -15,10 +16,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class ForgotPasswordActivity extends AppCompatActivity  {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     private EditText editEmail;
-    private Button resetButton;
+    private Button resetButton, returnButton;
 
     FirebaseAuth auth;
 
@@ -27,14 +28,26 @@ public class ForgotPasswordActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
+        //initialize the return to login menu button
+        returnButton = (Button) findViewById(R.id.loginreturn);
+        returnButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ForgotPasswordActivity.this, LoginActivity.class));
+            }
+        });
+
+
         //create field to capture user text input for email
-        editEmail = (EditText)  findViewById(R.id.useremailforgotpasswordvalue);
+        editEmail = (EditText) findViewById(R.id.useremailforgotpasswordvalue);
+
 
         //initialize the reset button
         resetButton = (Button) findViewById(R.id.resetpasswrodid);
         resetButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 resetPassword();
             }
 
@@ -45,6 +58,7 @@ public class ForgotPasswordActivity extends AppCompatActivity  {
 
 
     }
+
     private void resetPassword() {
         //create strings from user input & trim extraneous spaces
         String email = editEmail.getText().toString().trim();
@@ -71,10 +85,10 @@ public class ForgotPasswordActivity extends AppCompatActivity  {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
 
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Toast.makeText(ForgotPasswordActivity.this, "Check email for a password reset", Toast.LENGTH_LONG).show();
 
-                }else{
+                } else {
                     Toast.makeText(ForgotPasswordActivity.this, "Something is wrong. Try again.", Toast.LENGTH_LONG).show();
                 }
             }
